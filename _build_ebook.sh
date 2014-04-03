baeaead199d24e23f2096a67e99b9df7fe2e3ca4
Markus Amalthea Magnuson <markus.magnuson@gmail.com>
2014-04-04 00:08:44 +0200

    Working ebook generation, work-in-progress.

--- _build_ebook.sh
+++ _build_ebook.sh
@@ -6,14 +6,34 @@
 # This extra file sets an 'ebook' variable that in turn modifies the output to
 # hide a few elements, and such, that would othersize clutter the ebook.
 
-# TODO: Build for all languages.
+FILES="./index.html
+es-ES/index.html
+fi-FI/index.html
+sv-SE/index.html"
 
-# Generate .epub file for various readers.
-jekyll build --config _config.yml,_ebook.yml &&
-pandoc --output what-is-transhumanism.epub \
-       --epub-metadata=_site/epub.xml \
-       --epub-stylesheet=_epub.css \
-       _site/index.html
+# Build the site to make sure we are on the latest version.
+jekyll build &&
 
-# Generate .mobi file for Kindle.
-kindlegen what-is-transhumanism.epub
+for f in $FILES; do
+	DIRECTORY=${f/index.html/}
+	LANGUAGE_CODE=${f/\/index.html/}
+	if [ "$LANGUAGE_CODE" = "." ]; then
+		LANGUAGE_CODE="en-US"
+	fi
+
+	# Clean up HTML to make it more suitable for ebook generation.
+	python3 _clean_ebook.py "_site/$f" &&
+
+	# Generate .epub file for various readers.
+	pandoc --output "$DIRECTORY/what-is-transhumanism-$LANGUAGE_CODE.epub" \
+		--epub-cover-image="$DIRECTORY/img/_cover-$LANGUAGE_CODE.png" \
+		--epub-metadata="$DIRECTORY/_epub-$LANGUAGE_CODE.xml" \
+		--epub-stylesheet=_epub.css \
+		"_site/$DIRECTORY/index.html" &&
+
+	# DEBUG: Uncomment this to create an uncompressed EPUB folder, for inspection.
+	# unzip -o what-is-transhumanism.epub -d what-is-transhumanism-epub &&
+
+	# Generate .mobi file for Kindle.
+	kindlegen "$DIRECTORY/what-is-transhumanism-$LANGUAGE_CODE.epub"
+done
---
+++ _clean_ebook.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import sys
+from bs4 import BeautifulSoup
+
+
+if len(sys.argv) < 2:
+    sys.exit('No input file specified.')
+
+with open(sys.argv[1], 'r+') as f:
+    print('Preparing HTML for ebook generation...')
+
+    html = f.read()
+    soup = BeautifulSoup(html)
+
+    print('Stripping ids from h2 and h3 tags to avoid internal references.')
+    for selector in ('h2', 'h3'):
+        for t in soup.select(selector):
+            del t['id']
+
+    print('Stripping links inside headers, for the same reason.')
+    for selector in ('h2 a', 'h3 a'):
+        for t in soup.select(selector):
+            t['name'] = t['href'].replace('#', '')
+            del t['href']
+
+    print('Stripping some unnecessary divs.')
+    for selector in ('#wrapper', '#content', '#table-of-contents'):
+        for t in soup.select(selector):
+            t.unwrap()
+
+    print('Removing script, link and img tags entirely.')
+    for selector in ('script', 'link', 'img'):
+        for t in soup.select(selector):
+            t.decompose()
+
+    print('Removing some more unused elements entirely.')
+    for selector in ('#language-selector', '.print-only',
+        '.back-to-top', '#footer'):
+        for t in soup.select(selector):
+            t.decompose()
+
+    f.seek(0)
+    f.truncate()
+    f.write(soup.prettify())
