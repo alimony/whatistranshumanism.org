b7dd96dad179ad36027977fd1855341600db3e21
iinfin <48106228+iinfin@users.noreply.github.com>
2019-08-13 15:25:56 +0300

    edit script

--- ebook.py
+++ ebook.py
@@ -1,5 +1,14 @@
 # coding=utf-8
 import os
+import subprocess
 
-cwd = os.path.dirname(os.path.realpath(__file__))
-dst = os.path.join(cwd, "dist")
+directory = os.path.dirname(os.path.realpath(__file__))
+dist = os.path.join(directory, "dist")
+temp = os.path.join(directory, "tmp")
+
+html = os.path.join(dist, "index.html")
+epub = os.path.join(temp, "transhumanist-faq.epub")
+
+if __name__ == "__main__":
+    subprocess.run("pandoc -o " + epub + " dist/index.html --toc")
+    subprocess.run("kindlegen " + epub)
